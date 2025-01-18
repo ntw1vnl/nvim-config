7efa896a6118fdca48cef8959223ce7f62e1b274
ntw1vnl <antoine.vanel@tuta.io>
2025-01-18 10:49:20 +0100

    Updated telescope keymaps

--- lua/antoine/plugins/telescope.lua
+++ lua/antoine/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local builtin = require("telescope.builtin")
 
 		telescope.setup({
 			defaults = {
@@ -28,12 +29,14 @@ return {
 
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find strings in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find open buffers" })
-		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help tags" })
-		keymap.set("n", "<leader>ft", "<cmd>Todo Telescope<cr>", { desc = "Find TODO comments" })
+		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files in cwd" })
+		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find recent files" })
+		keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find strings in cwd" })
+		keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find open buffers" })
+		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find TODO comments" })
+		keymap.set("n", "<leader>fo", builtin.current_buffer_fuzzy_find, { desc = "Find in current buffer" })
+		keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find keymaps" })
 	end,
 }
